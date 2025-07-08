58c518cef72ad133a0f4c92a48093e4fb53375fa
NichoCM <36453140+NichoCM@users.noreply.github.com>
2025-07-08 12:04:31 -0400

    wip: scaffolding for navigation package

--- packages/example-native/react-native.config.js
+++ packages/example-native/react-native.config.js
@@ -10,10 +10,14 @@ module.exports = {
     '@idealyst/components': {
       root: path.join(__dirname, '../components'),
     },
+    '@idealyst/navigation': {
+      root: path.join(__dirname, '../navigation'),
+    },
   },
   // Additional configuration for workspace
   watchFolders: [
     path.resolve(__dirname, '../components'),
+    path.resolve(__dirname, '../navigation'),
     path.resolve(__dirname, '../../node_modules'),
   ],
 }; 
---
+++ packages/navigation/src/hooks/useNavigation/useNavigation.native.ts
@@ -0,0 +1,5 @@
+import { useNavigation as useNavigationNative } from '@react-navigation/native';
+
+export const useNavigation = () => {
+  return useNavigationNative();
+};
