e3b78da67ce43e22bc08b09568862079187ebff9
NichoCM <36453140+NichoCM@users.noreply.github.com>
2025-07-15 17:10:03 -0400

    fix: not CSS files are imported by default

--- packages/cli/templates/web/src/App.tsx
+++ packages/cli/templates/web/src/App.tsx
@@ -1,4 +1,3 @@
-import './App.css';
 import { BrowserRouter } from 'react-router-dom';
 import { ExampleStackRouter, } from '@idealyst/navigation/examples';
 import { NavigatorProvider } from '@idealyst/navigation';
